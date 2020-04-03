21dea907d7bcd31bd6c9748a4454c3fecf7ed345
TOSS <tiago.medice@gft.com>
2020-04-03 12:44:02 +0200

    ref

--- k8s-environment-service/src/main/java/com/medici/services/EnvironmentApplication.java
+++ k8s-environment-service/src/main/java/com/medici/services/EnvironmentApplication.java
@@ -3,10 +3,7 @@ package com.medici.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-
 @SpringBootApplication
-@EnableSwagger2WebMvc
 public class EnvironmentApplication {
 
 	public static void main(String[] args) {
