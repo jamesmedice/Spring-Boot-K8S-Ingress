9fbefe7bab74c67a3568666163aa483a65550a00
TOSS <tiago.medice@gft.com>
2020-04-03 12:28:44 +0200

    ref

--- k8s-environment-service/src/main/java/com/medici/services/environment/client/AdminClient.java
+++
@@ -1,8 +0,0 @@
-package com.medici.services.environment.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "admin")
-public interface AdminClient extends BaseFeignClient {
-
-}
--- k8s-environment-service/src/main/java/com/medici/services/environment/client/DepartmentClient.java
+++
@@ -1,8 +0,0 @@
-package com.medici.services.environment.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "department")
-public interface DepartmentClient extends BaseFeignClient {
-
-}
